1626a9f73392f13d53173431f37f8359d753ad7a
AdelShurrab <75363454+AdelShurrab@users.noreply.github.com>
2025-03-17 11:08:54 +0200

    Add files via upload

---
+++ app/src/main/java/com/example/homework1/HomeWork2.java
@@ -0,0 +1,13 @@
+package com.example.homework1;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomeWork2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_work2);
+    }
+}
